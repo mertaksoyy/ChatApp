e111ab664a05b49f7ceb0a8b48211e0000be01fa
Mert Aksoy <mrtaksy99@gmail.com>
2023-12-09 23:29:07 +0300

    Chat App

---
+++ chatapp_firebase/lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:877405393013:web:667669aba2eccc6476a83c";
+  static String apiKey = "AIzaSyAWngDMEt4F2H5w4NBpg7O_Yb5Jd5Y9TN8";
+  static String messagingSenderId = "877405393013";
+  static String projectId = "chatappflutter-2b8f7";
+}
